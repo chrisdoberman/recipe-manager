6a7d0a9e5e1a64b7f51a002c1bec140ae6c2143e
Chris Herb <chris_herb@hotmail.com>
2012-12-15 19:21:23 -0500

    updated usercontroller

--- src/main/java/com/internal/recipes/UserController.java
+++ src/main/java/com/internal/recipes/UserController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,4 +32,17 @@ public class UserController {
 		logger.info("Request to get all users.");
 		return userService.getAllUsers();
 	}
+	
+	@RequestMapping(value = "/{userName}", method = RequestMethod.GET)
+	public @ResponseBody User getRecipe(@PathVariable("userName") final String userName) {
+		logger.info("Request to get oa user with userName: {}", userName);
+		return userService.findByUserName(userName);
+	}
+	
+    @ExceptionHandler({UsernameNotFoundException.class})
+    ResponseEntity<String> handleNotFounds(Exception e) {
+    	logger.error(e.getMessage());
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 }
